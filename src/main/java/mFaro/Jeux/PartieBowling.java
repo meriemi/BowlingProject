c321e787e230fdd3392bf5dd8360917094d5137f
meriemi <meriem.farouj@gmail.com>
2016-11-14 23:43:46 +0100

    commit 1

---
+++ src/main/java/mFaro/Jeux/PartieBowling.java
@@ -0,0 +1,12 @@
+package mFaro.Jeux;
+
+/**
+ * Created by Mes documents on 14/11/2016.
+ */
+public  class  PartieBowling {
+    protected Jeu tabJeu[] = new Jeu[10];
+
+    public void demarrerJeu(Jeu jeu) {
+
+    }
+}
